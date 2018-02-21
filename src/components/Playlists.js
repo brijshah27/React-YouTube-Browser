ef4ab7bf2f5ee2fe3030af02152c046cd6169d73
brijshah27 <brijshah27@gmail.com>
2018-02-21 12:53:44 -0800

    display single playlist

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -1,37 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import config from '../config.js';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import Iframe from 'react-iframe';
 var queryString = require('query-string');
-
-// function showPlaylists (props) {
-//     //var playlist = queryString.parse(this.props.location.search);
-//     console.log(this.props)
-//     return (
-//     <div>
-//       <h1>Playlist goes here</h1>
-//     </div>
-//   )
-// }
+const API_KEY = config.API_KEY;
 
 class playlist extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            id : ''
+            id : '',
+            iframe :  null
         }
       }
     componentDidMount() {
         var playlist = queryString.parse(this.props.location.search);
-        console.log(playlist);
-
         this.setState({
-              id : playlist.channelID
-          })
+          id: playlist.channelId
+        });
+        console.log(playlist.channelID);
+        axios.get('https:www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails%2Cplayer&channelId='+playlist.channelID+'&maxResults=25&key='+API_KEY)
+        .then( (response) =>{
+          console.log("video id: "+response.data.items[0].player.embedHtml);
+          this.setState({
+            id: playlist.channelID,
+            iframe: response.data.items[1].player.embedHtml
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
     render(){
       //var playlist = queryString.parse(this.props.location.search);
        // var id = this.state.id;
         //console.log(id);
         return(
+          <div>
         <h1>playlist id is {this.state.id}</h1>
+        <div>
+        {ReactHtmlParser(this.state.iframe)}
+        </div>
+        </div>
         )
     }
 }
